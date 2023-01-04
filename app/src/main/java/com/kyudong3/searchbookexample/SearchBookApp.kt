f8c057fe259abbefbd492a40194c79b1e984facc
kyudong3 <kyudong3@gmail.com>
2023-01-05 00:41:15 +0900

    chore: DaggerApplication을 상속하는 SearchBookApp 생성

---
+++ app/src/main/java/com/kyudong3/searchbookexample/SearchBookApp.kt
@@ -0,0 +1,19 @@
+package com.kyudong3.searchbookexample
+
+import com.kyudong3.searchbookexample.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+
+class SearchBookApp : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    // DaggerApplication의 메서드 구현(재정의) - AppComponent는 AndroidInjector를 상속했기에 반환Type에 캐스팅
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        // AppComponent 인스턴스 반환
+        return DaggerAppComponent.create()
+    }
+}
