a92878dc5fcd446219ada54fd94e615442a6e379
kyudong3 <kyudong3@gmail.com>
2023-01-29 21:37:23 +0900

    feat: glide 추가하고 이미지 로딩 BindingAdapter 추가

---
+++ app/src/main/java/com/kyudong3/searchbookexample/utils/ImageViewBindingAdapter.kt
@@ -0,0 +1,27 @@
+package com.kyudong3.searchbookexample.utils
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+
+@BindingAdapter(
+    "imageUrl",
+    "errorImage",
+    "fallBackImage",
+    requireAll = false
+)
+fun AppCompatImageView.setImageView(
+    uri: String?,
+    errorImage: Drawable?,
+    fallBackImage: Drawable?
+) {
+    Glide.with(context)
+        .load(uri)
+        .error(errorImage)
+        .fallback(fallBackImage)
+        .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+        .into(this)
+}
