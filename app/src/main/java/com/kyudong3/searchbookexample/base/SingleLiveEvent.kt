7ca7dc6caf46aa1fc90165a0e4d61dcc5fd04f72
kyudong3 <kyudong3@gmail.com>
2023-02-13 10:58:09 +0900

    feat: SingleLiveEvent 추가

---
+++ app/src/main/java/com/kyudong3/searchbookexample/base/SingleLiveEvent.kt
@@ -0,0 +1,41 @@
+package com.kyudong3.searchbookexample.base
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+    private val isPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun setValue(value: T?) {
+        isPending.set(true)
+        super.setValue(value)
+    }
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        super.observe(owner) {
+            if (isPending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        }
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    companion object {
+        private const val TAG = "SingleLiveEvent"
+    }
+}
